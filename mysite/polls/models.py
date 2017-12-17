a6e3009d906c69c554e8b36353e1e4acee8429fa
sanchit-ahuja <sanchitahuja205@gmail.com>
2017-12-18 01:17:46 +0530

    Changes for the day 2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import datetime
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
@@ -9,6 +10,8 @@ class Question(models.Model):
     pub_date=models.DateTimeField('date published')
     def __str__(self):
         return self.question_text
+    def was_published_recently(self):
+        return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
 class Choice(models.Model):
     question=models.ForeignKey(Question,on_delete=models.CASCADE)
     choice_text=models.CharField(max_length=200)
