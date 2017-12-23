e5f411e6373e866391f532ef52ed2f352f8df7cd
sanchit-ahuja <sanchitahuja205@gmail.com>
2017-12-23 12:55:34 +0530

    a

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,8 +4,6 @@ from django.http import Http404
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
 from django.views import generic
-from django.views import generic
-from django.views import generic
 from django.utils import timezone
 # Create your views here.
 
