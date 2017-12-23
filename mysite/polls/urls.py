0ba26d2e8a485c632d2f62f3b5a2c0d2807472ff
sanchit-ahuja <sanchitahuja205@gmail.com>
2017-12-24 01:59:10 +0530

    s

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 from . import views
 
@@ -8,4 +8,5 @@ urlpatterns = [
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
     url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
